e921077a9555cb30b4b14ebe0da16d723c652b0d
Sudheer <sudheer7156@gmail.com>
2022-05-04 14:01:55 -0500

    created customerData.js to store mocked data

---
+++ src/api/customerData.js
@@ -0,0 +1,140 @@
+// eslint-disable-next-line
+export default function() {
+    // Mockup data to implement the program
+    return Promise.resolve(
+      [
+          {
+            custid: 1,
+            name: "Kartheek",
+            amt: 120,
+            transactionDt: "05-01-2021"
+          },
+          {
+            custid: 1,
+            name: "Kartheek",
+            amt: 75,
+            transactionDt: "05-21-2021"
+          },
+          {
+            custid: 1,
+            name: "Kartheek",
+            amt: 94,
+            transactionDt: "05-21-2021"
+          },
+          {
+            custid: 1,
+            name: "Kartheek",
+            amt: 10,
+            transactionDt: "06-01-2021"
+          },
+          {
+            custid: 1,
+            name: "Kartheek",
+            amt: 75,
+            transactionDt: "06-21-2021"
+          },
+          {
+            custid: 1,
+            name: "Kartheek",
+            amt: 200,
+            transactionDt: "07-01-2021"
+          },
+          {
+            custid: 1,
+            name: "Kartheek",
+            amt: 1,
+            transactionDt: "07-04-2021"
+          },
+          {
+            custid: 1,
+            name: "Kartheek",
+            amt: 80,
+            transactionDt: "07-03-2021"
+          },
+          {
+            custid: 1,
+            name: "Kartheek",
+            amt: 224,
+            transactionDt: "07-21-2021"
+          },
+          {
+            custid: 2,
+            name: "Vamshi",
+            amt: 125,
+            transactionDt: "05-01-2021"
+          },
+          {
+            custid: 2,
+            name: "Vamshi",
+            amt: 75,
+            transactionDt: "05-21-2021"
+          },
+          {
+            custid: 2,
+            name: "Vamshi",
+            amt: 10,
+            transactionDt: "06-01-2021"
+          },
+          {
+            custid: 2,
+            name: "Vamshi",
+            amt: 75,
+            transactionDt: "06-21-2021"
+          },
+          {
+            custid: 2,
+            name: "Vamshi",
+            amt: 200,
+            transactionDt: "07-01-2021"
+          },
+          {
+            custid: 2,
+            name: "Vamshi",
+            amt: 224,
+            transactionDt: "07-21-2021"
+          },
+          {
+            custid: 3,
+            name: "Shabarish",
+            amt: 120,
+            transactionDt: "06-21-2021"
+          },
+          {
+            custid: 3,
+            name: "Shabarish",
+            amt: 135,
+            transactionDt: "06-30-2021"
+          },
+          {
+            custid: 3,
+            name: "Anusha",
+            amt: 246,
+            transactionDt: "07-05-2021"
+          },
+          {
+            custid: 3,
+            name: "Anusha",
+            amt: 168,
+            transactionDt: "07-16-2021"
+          },
+          {
+            custid: 3,
+            name: "Anusha",
+            amt: 293,
+            transactionDt: "07-26-2021"
+          },
+          {
+            custid: 3,
+            name: "Anusha",
+            amt: 150,
+            transactionDt: "08-10-2021"
+          },
+          {
+            custid: 3,
+            name: "Anusha",
+            amt: 36,
+            transactionDt: "08-17-2021"
+          }
+      ]
+    );
+  };
